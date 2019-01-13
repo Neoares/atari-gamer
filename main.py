896159c460794da8376aa9c2b36e270393885d36
Alex Castrelo <neoares23@gmail.com>
2019-01-13 13:38:52 +0100

    Clean code, keeping only the non-DQN algorithm and agent.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 
-from src.agent import Agent, DQNAgent
+from src.agent import Agent
 
 
 def train(env, agent, episodes, render=False):
@@ -24,8 +24,10 @@ def train(env, agent, episodes, render=False):
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
             agent.update_q_table(state, action, reward, next_state, done)
+
             if render:
                 env.render()
+
             state = next_state
             total_score[i] += reward
 
@@ -53,73 +55,18 @@ def train(env, agent, episodes, render=False):
     return total_score
 
 
-def train_DQN(env, agent, episodes, render=False):
-    total_epochs, total_penalties = 0, 0
-
-    total_score = np.zeros(episodes)
-
-    epsilon_decay = 2 * (agent.epsilon - agent.epsilon_min) / episodes
-    alpha_decay = (agent.alpha - agent.alpha_min) / episodes
-    gamma_decay = (agent.gamma - agent.gamma_min) / episodes
-
-    batch_size = 32
-
-    for i in range(episodes):
-        state = env.reset().reshape((1, agent.state_size))
-        epochs = 0
-        while True:
-            epochs += 1
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            next_state = next_state.reshape((1, agent.state_size))
-
-            agent.remember(state, action, reward, next_state, done)
-            if render:
-                env.render()
-
-            state = next_state
-            total_score[i] += reward
-
-            if done:
-                if i % 10 == 0:
-                    print("episode {}/{}, score: {}, epsilon: {:.2}, gamma: {:.2}, alpha: {:.2}".format(i, episodes,
-                            total_score[i], float(agent.epsilon), float(agent.gamma), float(agent.alpha)))
-                break
-
-        agent.epsilon = max(agent.epsilon_min, agent.epsilon - epsilon_decay)
-        agent.alpha = max(agent.alpha_min, agent.alpha - alpha_decay)
-        agent.gamma = max(agent.gamma_min, agent.gamma - gamma_decay)
-
-        total_epochs += epochs
-        # experience replay
-        if len(agent.memory) > batch_size:
-            agent.replay(batch_size)
-
-    print("TOTAL SCORES:")
-    print(total_score)
-    print(f"Results after {episodes} episodes:")
-    print(f"Average timesteps per episode: {total_epochs / episodes}")
-    plt.plot(range(episodes), [np.sum(total_score[0:i+1])/(i+1) for i in range(len(total_score))], 'b')
-    plt.show()
-
-    # Close the env and write monitor result info to disk
-    env.env.close()
-
-    return total_score
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('env_id', nargs='?', default='Breakout-ram-v0', help='Select the environment to run')
+    parser.add_argument('env_id', nargs='?', default='Taxi-v2', help='Select the environment to run')
     args = parser.parse_args()
 
     env = gym.make(args.env_id)
-    #env.seed(42)
-    # agent = Agent(env.observation_space.n, env.action_space.n)
-    agent = DQNAgent(env.observation_space.shape[0], env.action_space.n)
+    # env.seed(42)
 
-    train_score = train_DQN(env, agent, 10)
+    agent = Agent(env.observation_space.n, env.action_space.n)
+    train_score = train(env, agent, 100000)
     agent.prod = True
-    test_score = train_DQN(env, agent, 100)
+    test_score = train(env, agent, 100)
+
     print("Production algorithm mean:", test_score.mean())
     print("Last 100 games mean:", test_score[-100:].mean())
--- src/agent.py
+++ src/agent.py
@@ -1,10 +1,5 @@
 import numpy as np
 import random
-from collections import deque
-
-from keras import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from keras.optimizers import Adam
 
 
 class Agent(object):
@@ -36,75 +31,3 @@ class Agent(object):
             return np.argmax(self.Q_table[state])
         else:
             return random.randrange(self.action_size)
-
-
-class DQNAgent:
-    def __init__(self, state_size, action_size):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.memory = deque(maxlen=10000)
-
-        self.alpha = .5
-        self.alpha_min = 0.001
-
-        self.gamma = 0.99
-        self.gamma_min = 0.1
-
-        self.epsilon = 1
-        self.epsilon_min = 0.1
-
-        self.model = self._build_model()
-
-    def _build_model(self):
-        model = Sequential()
-
-        # model.add(Conv2D(32, (7, 7), padding='same', input_shape=self.state_size, activation='relu'))
-        # model.add(Conv2D(32, (7, 7), padding='same', activation='relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Conv2D(64, (5, 5), padding='same', input_shape=self.state_size, activation='relu'))
-        # model.add(Conv2D(64, (5, 5), padding='same', activation='relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Conv2D(64, (3, 3), padding='same', input_shape=self.state_size, activation='relu'))
-        # model.add(Conv2D(64, (3, 3), padding='same', activation='relu'())
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Conv2D(128, (3, 3), padding='same', input_shape=self.state_size, activation='relu'))
-        # model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-
-        # model.add(Flatten())
-        model.add(Dense(512, activation='relu', input_shape=(self.state_size,)))
-        model.add(Dense(512, activation='relu'))
-
-        model.add(Dense(self.action_size, activation='softmax'))
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=Adam(lr=self.alpha))
-        model.summary()
-        return model
-
-    def act(self, state):
-        # select random action with prob=epsilon else action=maxQ
-        if np.random.random() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])
-
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done))
-
-    def replay(self, batch_size):
-        # sample random transitions
-        minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
-            target = reward
-            if not done:
-                Q_next = self.model.predict(next_state)[0]
-                target = (reward + self.gamma * np.amax(Q_next))
-
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-
-            # train network
-            self.model.fit(state, target_f, epochs=1, verbose=0)
